314c0ba5169514f5d338d029b5bddced2f957e8f
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-06-20 15:26:28 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: "365templates-overhaul",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
